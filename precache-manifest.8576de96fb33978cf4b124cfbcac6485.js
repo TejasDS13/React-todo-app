fccc73be9f030ce1eb035048c1efe8fbe4d1bb10
Tejas <darkgamer1230909@gmail.com>
2026-07-05 15:09:53 +0530

    Updates

---
+++ precache-manifest.8576de96fb33978cf4b124cfbcac6485.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160a9b20c6c42d4f679f36293dd0318f",
+    "url": "/TejasDS13/React-todo-app/index.html"
+  },
+  {
+    "revision": "43f4cec412f6647f67df",
+    "url": "/TejasDS13/React-todo-app/static/css/main.82c9f597.chunk.css"
+  },
+  {
+    "revision": "ad414979b372226aef39",
+    "url": "/TejasDS13/React-todo-app/static/js/2.63d1df5b.chunk.js"
+  },
+  {
+    "revision": "43f4cec412f6647f67df",
+    "url": "/TejasDS13/React-todo-app/static/js/main.7ecc48a5.chunk.js"
+  },
+  {
+    "revision": "e952f8921fc133aea791",
+    "url": "/TejasDS13/React-todo-app/static/js/runtime-main.2bcdc6b4.js"
+  }
+]);
